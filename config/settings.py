8d18ffc1244639f29203cb365123621e9357697d
Dipika Patil <dipikapatil1805@gmail.com>
2026-08-03 12:38:48 +0530

    Fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(=ii)fudnb)ng!=9939=-nfvmvwo#6g&9gx50lxfqbuqp)6+hb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hrbot-78yd.onrender.com"]
 
 
 # Application definition
