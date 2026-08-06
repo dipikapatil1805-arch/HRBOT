fcb68ab11039e1dc84ea5b887f461c8302066612
Dipika Patil <dipikapatil1805@gmail.com>
2026-08-06 14:31:44 +0530

    Fix static files and add WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-(=ii)fudnb)ng!=9939=-nfvmvwo#6g&9gx50lxfqbuqp)6+hb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["hrbot-78yd.onrender.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "hrbot-78yd.onrender.com",
+]
 
 
 # Application definition
@@ -49,6 +53,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,12 +124,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
